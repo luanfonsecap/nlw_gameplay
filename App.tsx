b37583559aa29bfce7218ed196c39862ee6bb3e9
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-25 15:24:45 -0300

    feat: :sparkles: add google fonts and persist splash screen
    added new fonts and persist splash until load fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading'
 
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
+  const [fontIsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if (!fontIsLoaded) {
+    return <AppLoading />
+  }
+
   return <SignIn />;
 }
