5fa6df06c066160fbaef90a3b684b41245ea4597
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-25 14:05:26 -0300

    feat: create button with icon component
    create a component that accepts a title and have a discord icon

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 56,
+    height: 56,
+    borderRightWidth: 1,
+    borderColor: theme.colors.line,
+  },
+  icon: {
+    width: 24,
+    height: 18,
+  },
+  title: {
+    flex: 1,
+    color: theme.colors.reading,
+    fontSize: 15,
+    textAlign: "center",
+  },
+});
