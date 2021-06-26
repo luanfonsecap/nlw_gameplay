ed3c46f6b7215aa6a4ffc18a4faabe82b024c193
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-25 23:46:59 -0300

    refactor: :recycle: change TouchableOpacity for RectButton in ButtonAdd
    changed generic component for button to specific RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-import {
-  TouchableOpacity,
-  View,
-  Image,
-  Text,
-  TouchableOpacityProps,
-} from "react-native";
+import { View, Image, Text } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
 
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
   title: string;
 };
 
 function ButtonIcon({ title, ...rest }: Props) {
+  const navigation = useNavigation();
+
+  const handleSignIn = () => {
+    navigation.navigate("Home");
+  };
+
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} onPress={handleSignIn} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
 
---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { Avatar } from "../Avatar";
+import { ButtonAdd } from "../ButtonAdd";
+import { styles } from "./styles";
+
+function Profile() {
+  return (
+    <View style={styles.container}>
+      <Avatar urlAvatar="https://www.github.com/luanfonsecap.png" />
+
+      <View style={styles.info}>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá,</Text>
+          <Text style={styles.username}>Luan</Text>
+        </View>
+        <View>
+          <Text style={styles.message}>Hoje é dia de vitória</Text>
+        </View>
+      </View>
+
+      <ButtonAdd />
+    </View>
+  );
+}
+
+export { Profile };
