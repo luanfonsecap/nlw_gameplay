f9d53f50abe93e889da85ba4f54be1c6b31ff097
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-26 20:28:02 -0300

    refactor: :recycle: adjust linear gradient in Category component and add hasCheckBox prop

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,33 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
-import { SvgProps } from "react-native-svg";
-import { LinearGradient } from "expo-linear-gradient";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { SvgProps } from 'react-native-svg';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import { styles } from "./styles";
-import { theme } from "../../global/styles/theme";
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
 type Props = RectButtonProps & {
 	title: string;
 	icon: React.FC<SvgProps>;
+	hasCheckBox?: boolean;
 	checked?: boolean;
 };
 
-function Category({ title, icon: Icon, checked = false, ...rest }: Props) {
-	const { secondary40, secondary70 } = theme.colors;
+function Category({
+	title,
+	icon: Icon,
+	checked = false,
+	hasCheckBox = false,
+	...rest
+}: Props) {
+	const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
 	return (
 		<RectButton {...rest}>
 			<LinearGradient
 				style={styles.container}
-				colors={[secondary40, secondary70]}
-			>
-				<View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-					<View style={checked ? styles.checked : styles.check} />
+				colors={[secondary40, secondary70]}>
+				<LinearGradient
+					style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+					colors={[checked ? secondary85 : secondary50, secondary40]}>
+					{hasCheckBox ? (
+						<View style={checked ? styles.checked : styles.check} />
+					) : null}
+
 					<Icon width={48} height={48} />
 
 					<Text style={styles.title}>{title}</Text>
-				</View>
+				</LinearGradient>
 			</LinearGradient>
 		</RectButton>
 	);
--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -1,12 +1,12 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles/theme";
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
 	container: {
 		width: 104,
 		height: 120,
-		justifyContent: "center",
-		alignItems: "center",
+		justifyContent: 'center',
+		alignItems: 'center',
 		borderRadius: 8,
 		marginRight: 8,
 	},
@@ -15,30 +15,32 @@ export const styles = StyleSheet.create({
 		height: 116,
 		backgroundColor: theme.colors.secondary40,
 		borderRadius: 8,
-		justifyContent: "space-between",
-		alignItems: "center",
-		paddingVertical: 7,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingVertical: 20,
 	},
 	checked: {
+		position: 'absolute',
+		top: 7,
+		right: 7,
 		width: 10,
 		height: 10,
 		backgroundColor: theme.colors.primary,
-		alignSelf: "flex-end",
-		marginRight: 7,
 		borderRadius: 3,
 	},
 	check: {
+		position: 'absolute',
+		top: 7,
+		right: 7,
 		width: 12,
 		height: 12,
 		backgroundColor: theme.colors.secondary100,
-		alignSelf: "flex-end",
-		marginRight: 7,
 		borderColor: theme.colors.secondary50,
 		borderWidth: 2,
 		borderRadius: 3,
 	},
 	title: {
-		fontFamily: theme.fonts.title500,
+		fontFamily: theme.fonts.title700,
 		color: theme.colors.heading,
 		fontSize: 15,
 	},
