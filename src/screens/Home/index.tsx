aa25bde629619e850a74ddd3047311fb19885a51
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-27 11:13:21 -0300

    feat: :clown_face: add more one appointment mock

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -26,6 +26,19 @@ function Home() {
 			description:
 				'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
 		},
+		{
+			id: '2',
+			guild: {
+				id: '1',
+				name: 'Lendários',
+				icon: null,
+				owner: true,
+			},
+			category: '1',
+			date: '26/06 ás 20:40h',
+			description:
+				'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+		},
 	];
 
 	const handleSelectCategory = (categoryId: string) => {
