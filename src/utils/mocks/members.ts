3356f9fb062a59fc7c657df06a6d1700b308fa39
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-27 21:25:31 -0300

    feat: :clown_face: add mock for members of server
    creadted mock for members in server to design layout with data

---
+++ src/utils/mocks/members.ts
@@ -0,0 +1,20 @@
+export const members = [
+	{
+		id: '1',
+		username: 'Luan',
+		avatar_url: 'https://github.com/luanfonsecap.png',
+		status: 'online',
+	},
+	{
+		id: '2',
+		username: 'Diego',
+		avatar_url: 'https://github.com/diego3g.png',
+		status: 'offline',
+	},
+	{
+		id: '3',
+		username: 'Rodrigo',
+		avatar_url: 'https://github.com/rodrigorgtic.png',
+		status: 'online',
+	},
+];
