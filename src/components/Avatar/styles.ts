8b6279a88dae127353b39ad326f0d33356acd641
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-27 21:28:31 -0300

    refactor: :recycle: add margin to Avatar component
    now Avatar component have your own margin right

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,17 +1,18 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles/theme";
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 50,
-    height: 50,
-    borderRadius: 8,
-  },
-  avatar: {
-    borderRadius: 8,
-    width: 46,
-    height: 46,
-  },
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 50,
+		height: 50,
+		borderRadius: 8,
+		marginRight: 16,
+	},
+	avatar: {
+		borderRadius: 8,
+		width: 46,
+		height: 46,
+	},
 });
