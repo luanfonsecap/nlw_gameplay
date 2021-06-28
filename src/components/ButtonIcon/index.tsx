0bb50e181c2f2eead00c7d665093e296a03fd18f
Luan Fonseca <luan.fonseca@vamosparcelar.com.br>
2021-06-27 21:25:25 -0300

    fix: :bug: correct locate to function sign in
    moved function to redirect user to sign in to correct page

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,31 +1,24 @@
-import React from "react";
-import { View, Image, Text } from "react-native";
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
-import DiscordImg from "../../assets/discord.png";
-import { styles } from "./styles";
+import DiscordImg from '../../assets/discord.png';
+import { styles } from './styles';
 
 type Props = RectButtonProps & {
-  title: string;
+	title: string;
 };
 
 function ButtonIcon({ title, ...rest }: Props) {
-  const navigation = useNavigation();
+	return (
+		<RectButton style={styles.container} {...rest}>
+			<View style={styles.iconWrapper}>
+				<Image source={DiscordImg} style={styles.icon} />
+			</View>
 
-  const handleSignIn = () => {
-    navigation.navigate("Home");
-  };
-
-  return (
-    <RectButton style={styles.container} onPress={handleSignIn} {...rest}>
-      <View style={styles.iconWrapper}>
-        <Image source={DiscordImg} style={styles.icon} />
-      </View>
-
-      <Text style={styles.title}>{title}</Text>
-    </RectButton>
-  );
+			<Text style={styles.title}>{title}</Text>
+		</RectButton>
+	);
 }
 
 export { ButtonIcon };
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -8,6 +9,12 @@ import { Background } from '../../components/Background';
 import { styles } from './styles';
 
 function SignIn() {
+	const navigation = useNavigation();
+
+	const handleSignIn = () => {
+		navigation.navigate('Home');
+	};
+
 	return (
 		<Background>
 			<View style={styles.container}>
@@ -27,7 +34,11 @@ function SignIn() {
 						favoritos com seus amigos
 					</Text>
 
-					<ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+					<ButtonIcon
+						title="Entrar com Discord"
+						activeOpacity={0.7}
+						onPress={handleSignIn}
+					/>
 				</View>
 			</View>
 		</Background>
