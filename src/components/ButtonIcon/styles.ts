76cc749e791954b7172fd1f0ba369c6f2b3dde14
Luan Fonseca <luanfonsecap@icloud.com>
2021-07-03 17:57:14 -0300

    style: :lipstick: update button icon styles file

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,32 +1,33 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-import { theme } from "../../global/styles/theme";
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: 56,
-    backgroundColor: theme.colors.primary,
-    borderRadius: 8,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  iconWrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 56,
-    height: 56,
-    borderRightWidth: 1,
-    borderColor: theme.colors.line,
-  },
-  icon: {
-    width: 24,
-    height: 18,
-  },
-  title: {
-    flex: 1,
-    color: theme.colors.heading,
-    fontSize: 15,
-    textAlign: "center",
-  },
+	container: {
+		width: '100%',
+		height: 56,
+		backgroundColor: theme.colors.primary,
+		borderRadius: 8,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	iconWrapper: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 56,
+		height: 56,
+		borderRightWidth: 1,
+		borderColor: theme.colors.line,
+	},
+	icon: {
+		width: 24,
+		height: 18,
+	},
+	title: {
+		flex: 1,
+		color: theme.colors.heading,
+		fontFamily: theme.fonts.text500,
+		fontSize: 15,
+		textAlign: 'center',
+	},
 });
