2da92089a0b0e1be76304203a18a6c2a0603a850
Luan Fonseca <luanfonsecap@icloud.com>
2021-07-04 11:38:59 -0300

    feat: :sparkles: create a AppointmentCreate screen
    created screen to create a appointment in a guild

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { SignIn } from '../screens/SignIn';
 import { Home } from '../screens/Home';
 import { AppointmentDetails } from '../screens/AppointmentDetails';
+import { AppointmentCreate } from '../screens/AppointmentCreate';
 
 import { theme } from '../global/styles/theme';
 
@@ -21,6 +22,7 @@ function AuthRoutes() {
 			<Screen name="SignIn" component={SignIn} />
 			<Screen name="Home" component={Home} />
 			<Screen name="AppointmentDetails" component={AppointmentDetails} />
+			<Screen name="AppointmentCreate" component={AppointmentCreate} />
 		</Navigator>
 	);
 }
---
+++ src/screens/AppointmentCreate/index.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import {
+	View,
+	Text,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
+import { Header } from '../../components/Header';
+import { CategorySelect } from '../../components/CategorySelect';
+import { GuildIcon } from '../../components/GuildIcon';
+import { SmallInput } from '../../components/SmallInput';
+import { TextArea } from '../../components/TextArea';
+import { Button } from '../../components/Button';
+import { ModalView } from '../../components/ModalView';
+import { Guilds } from '../../screens/Guilds';
+import { GuildProps } from '../../components/Guild';
+
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
+
+function AppointmentCreate() {
+	const [category, setCategory] = useState('');
+	const [openModalGuilds, setOpenModalGuilds] = useState(false);
+	const [selectedGuild, setSelectedGuild] = useState<GuildProps>(
+		{} as GuildProps,
+	);
+
+	const handleOpenModalGuilds = () => {
+		setOpenModalGuilds(true);
+	};
+
+	const handleSelectGuild = (guildSelected: GuildProps) => {
+		setSelectedGuild(guildSelected);
+		setOpenModalGuilds(false);
+	};
+
+	return (
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			style={styles.container}>
+			<ScrollView>
+				<Header title="Agendar partida" />
+
+				<Text
+					style={[
+						styles.label,
+						{ marginLeft: 24, marginTop: 36, marginBottom: 18 },
+					]}>
+					Categoria
+				</Text>
+
+				<CategorySelect
+					hasCheckBox
+					setCategory={setCategory}
+					categorySelected={category}
+				/>
+
+				<View style={styles.form}>
+					<RectButton onPress={handleOpenModalGuilds}>
+						<View style={styles.select}>
+							{selectedGuild.icon ? (
+								<View style={styles.image} />
+							) : (
+								<GuildIcon />
+							)}
+
+							<View style={styles.selectBody}>
+								<Text style={styles.label}>
+									{selectedGuild.name
+										? selectedGuild.name
+										: 'Selecione um servidor'}
+								</Text>
+							</View>
+
+							<Feather
+								name="chevron-right"
+								color={theme.colors.heading}
+								size={18}
+							/>
+						</View>
+					</RectButton>
+
+					<View style={styles.field}>
+						<View>
+							<Text style={styles.label}>Dia e mês</Text>
+
+							<View style={styles.column}>
+								<SmallInput maxLength={2} />
+								<Text style={styles.divider}>/</Text>
+								<SmallInput maxLength={2} />
+							</View>
+						</View>
+
+						<View>
+							<Text style={styles.label}>Hora e minuto</Text>
+
+							<View style={styles.column}>
+								<SmallInput maxLength={2} />
+								<Text style={styles.divider}>:</Text>
+								<SmallInput maxLength={2} />
+							</View>
+						</View>
+					</View>
+
+					<View style={[styles.field, { marginBottom: 12 }]}>
+						<Text style={styles.label}>Descrição</Text>
+
+						<Text style={styles.charactersLimit}>Max 100 caracteres</Text>
+					</View>
+
+					<TextArea
+						multiline
+						maxLength={100}
+						numberOfLines={5}
+						autoCorrect={false}
+					/>
+
+					<View style={styles.footer}>
+						<Button title="Agendar" />
+					</View>
+				</View>
+			</ScrollView>
+
+			<ModalView visible={openModalGuilds}>
+				<Guilds handleSelectGuild={handleSelectGuild} />
+			</ModalView>
+		</KeyboardAvoidingView>
+	);
+}
+
+export { AppointmentCreate };
