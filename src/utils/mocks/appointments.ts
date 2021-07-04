bd2535465b10add0ddf7549ae13de9dde57fcee7
Luan Fonseca <luanfonsecap@icloud.com>
2021-07-04 11:55:03 -0300

    refactor: :clown_face: add more mock items
    add more mocks for guilds and appointments

--- src/utils/mocks/appointments.ts
+++ src/utils/mocks/appointments.ts
@@ -25,4 +25,82 @@ export const appointments = [
 		description:
 			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
 	},
+	{
+		id: '3',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
+	{
+		id: '4',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
+	{
+		id: '5',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
+	{
+		id: '6',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
+	{
+		id: '7',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
+	{
+		id: '8',
+		guild: {
+			id: '1',
+			name: 'Lendários',
+			icon: null,
+			owner: true,
+		},
+		category: '1',
+		date: '26/06 ás 20:40h',
+		description:
+			'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+	},
 ];
--- src/utils/mocks/guilds.ts
+++ src/utils/mocks/guilds.ts
@@ -5,4 +5,52 @@ export const guilds = [
 		icon: null,
 		owner: true,
 	},
+	{
+		id: '2',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '3',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '4',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '5',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '6',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '7',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '8',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
+	{
+		id: '9',
+		name: 'Lendários',
+		icon: null,
+		owner: true,
+	},
 ];
