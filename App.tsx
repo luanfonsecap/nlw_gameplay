f7f7fefff44f9939593be2f7a4f54951f9fb3a7c
Luan Fonseca <luanfonsecap@icloud.com>
2021-07-10 17:37:29 -0300

    feat: :sparkles: add auth provider to routes

--- App.tsx
+++ App.tsx
@@ -1,36 +1,43 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import React from 'react';
+import { StatusBar, LogBox } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import {
-  Rajdhani_500Medium,
-  Rajdhani_700Bold,
-} from "@expo-google-fonts/rajdhani";
-import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
+	Rajdhani_500Medium,
+	Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
-import { Background } from "./src/components/Background";
-import { Routes } from "./src/routes";
+import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
+import { AuthProvider } from './src/hooks/auth';
+
+LogBox.ignoreLogs([
+	'You are not currently signed in to Expo on your development machine',
+]);
 
 export default function App() {
-  const [fontIsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Rajdhani_500Medium,
-    Rajdhani_700Bold,
-  });
+	const [fontIsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+		Rajdhani_500Medium,
+		Rajdhani_700Bold,
+	});
 
-  if (!fontIsLoaded) {
-    return <AppLoading />;
-  }
+	if (!fontIsLoaded) {
+		return <AppLoading />;
+	}
 
-  return (
-    <Background>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Routes />
-    </Background>
-  );
+	return (
+		<Background>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<AuthProvider>
+				<Routes />
+			</AuthProvider>
+		</Background>
+	);
 }
